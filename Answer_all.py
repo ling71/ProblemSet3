d1c35db9e9694ca4ea3f0513de528594621c0da4
Qinyin Ling <lingqinyin@login2.ufhpc>
2018-10-20 09:54:52 -0400

    merged answer code upload

---
+++ Answer_all.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Question 1
+data = open('CO-OPS__8729108__wl.csv')
+part_1 = data.readlines()[1:424]
+data = open('CO-OPS__8729108__wl.csv')
+part_2 = data.readlines()[425:]
+data_merged = part_1 + part_2
+
+water_level_list = []
+for line in data_merged:
+    delimiter = ','
+    water_level = line.split(delimiter)
+    floats=float(water_level[1])
+    water_level_list.append(floats)
+
+largest = None
+for i in water_level_list:
+    if largest is None or i > largest:
+        largest = i
+print('Highest_Water_Level',largest)
+
+for line in data_merged:
+    if line.find(str(largest)) == -1: continue
+    delimiter = ','
+    date_time = line.split(delimiter)
+print('Date_time',date_time[0],'\n')
+
+
+#Question 2
+df = pd.read_csv("CO-OPS__8729108__wl.csv")
+df = df.rename(columns=lambda x: x.replace(" ",""))
+print('Highest_Water_Level',df['WaterLevel'].max())
+print ('Date_Time',df.ix[df.WaterLevel.idxmax(), 'DateTime'],'\n')
+
+
+#Question 3
+df = pd.read_csv("CO-OPS__8729108__wl.csv")
+df = df.rename(columns=lambda x: x.replace(" ",""))
+df['Difference'] = df['WaterLevel'] - df['WaterLevel'].shift(+1)
+print ('Highest_Rise',df.Difference.max().round(3))
+Line_Number = df.Difference.idxmax()
+print('Date_Time_Before_Rise',df.ix[Line_Number-1,[0]].values[0])
+print('Water_Level_Before_Rise',round(float(df.ix[Line_Number-1,[1]].values[0]),3))
+print ('Date_Time_After_Rise',df.ix[Line_Number,'DateTime'])
+print ('Water_Level_After_Rise',df.ix[Line_Number,'WaterLevel'],'\n')
+
+
+#Question 4
+df = pd.read_csv("CO-OPS__8729108__wl.csv")
+df = df.rename(columns=lambda x: x.replace(" ",""))
+plt.title('Water level observations from Panama City during Hurricane Michael', fontsize=12)
+plt.xlabel("Measurement Interval Count (6-min Interval)", fontsize=10)
+plt.ylabel('Water Level (Meters)', fontsize=10)
+plt.axis([0, 1.1 * len(df.axes[0]), 0, 1.1 * df['WaterLevel'].max()])
+counts = df['WaterLevel'].values
+plt.scatter(df.index,counts,c =200 * counts, s = 0.5)
+plt.show()
+plt.savefig("plot.png")
